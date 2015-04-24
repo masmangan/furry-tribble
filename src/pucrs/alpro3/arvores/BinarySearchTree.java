9348f126aca242380beca7b632c4d746732beb19
Marco Mangan <marco.mangan@gmail.com>
2015-04-24 20:06:19 -0300

    add simples

--- src/pucrs/alpro3/arvores/BinarySearchTree.java
+++ src/pucrs/alpro3/arvores/BinarySearchTree.java
@@ -4,15 +4,58 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class BinarySearchTree {
 
+	class Node {
+		int value;
+		Node left, right;
+	}
+	
+	private int count;
+	
+	private Node root;
+	
+	public BinarySearchTree() {
+		root = null;
+		count = 0;
+	}
+	
+	public boolean isEmpty() {
+		return root == null;
+	}
 	
 	public int size() {
-		throw new NotImplementedException();
+		return count;
 	}
 	
 	public void add(int v) {
+		root = add(root, v);
+		//
+		count++;
 		throw new NotImplementedException();
 	}
 	
+	private Node add(Node node, int v) {
+		// TODO Se a árvore está vazia
+		// cria um novo nodo
+		// e guarda o valor v
+		if (node == null) {
+			Node newNode = new Node();
+			newNode.value = v;
+			newNode.left = null;
+			newNode.right = null;
+			return newNode;
+		} else {
+			if (v < node.value) {
+				node.left = add(node.left, v);
+			} else if (v > node.value) {
+				node.right = add(node.right, v);
+			}
+		}
+		// TODO Se a árvore NÃO está vazia
+		// escolher o lado direito ou o lado esquerdo
+		// para continuar a inserção
+		return null;
+	}
+
 	public boolean contains(int v) {
 		throw new NotImplementedException();
 	}
