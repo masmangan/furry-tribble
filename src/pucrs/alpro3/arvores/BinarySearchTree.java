26496d72c0cae29fbb122ea2d4cc85f4cbf4b39e
Marco Mangan <marco.mangan@gmail.com>
2015-04-24 19:46:18 -0300

    API

--- src/pucrs/alpro3/arvores/BinarySearchTree.java
+++ src/pucrs/alpro3/arvores/BinarySearchTree.java
@@ -1,5 +1,20 @@
 package pucrs.alpro3.arvores;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 public class BinarySearchTree {
 
+	
+	public int size() {
+		throw new NotImplementedException();
+	}
+	
+	public void add(int v) {
+		throw new NotImplementedException();
+	}
+	
+	public boolean contains(int v) {
+		throw new NotImplementedException();
+	}
+	
 }
