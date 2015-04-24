e57f46058f9f31c5f6d061ceb2af68a6d87f3a87
Marco Mangan <marco.mangan@gmail.com>
2015-04-24 20:21:26 -0300

    add completo

--- src/pucrs/alpro3/arvores/BinarySearchTree.java
+++ src/pucrs/alpro3/arvores/BinarySearchTree.java
@@ -7,57 +7,65 @@ public class BinarySearchTree {
 	class Node {
 		int value;
 		Node left, right;
+
+		public Node(int value) {
+			this.value = value;
+			left = right = null;
+			count++;
+		}
+
 	}
-	
+
 	private int count;
-	
+
 	private Node root;
-	
+
 	public BinarySearchTree() {
 		root = null;
 		count = 0;
 	}
-	
+
 	public boolean isEmpty() {
 		return root == null;
 	}
-	
+
 	public int size() {
 		return count;
 	}
-	
+
 	public void add(int v) {
 		root = add(root, v);
-		//
-		count++;
-		throw new NotImplementedException();
 	}
-	
+
 	private Node add(Node node, int v) {
-		// TODO Se a árvore está vazia
-		// cria um novo nodo
-		// e guarda o valor v
-		if (node == null) {
-			Node newNode = new Node();
-			newNode.value = v;
-			newNode.left = null;
-			newNode.right = null;
-			return newNode;
-		} else {
-			if (v < node.value) {
-				node.left = add(node.left, v);
-			} else if (v > node.value) {
-				node.right = add(node.right, v);
-			}
-		}
-		// TODO Se a árvore NÃO está vazia
-		// escolher o lado direito ou o lado esquerdo
-		// para continuar a inserção
-		return null;
+
+		if (node == null)
+			return new Node(v);
+
+		if (v < node.value)
+			node.left = add(node.left, v);
+		else if (v > node.value)
+			node.right = add(node.right, v);
+		else
+			throw new IllegalArgumentException("A chave está cadastrada:" + v);
+
+		return node;
 	}
 
 	public boolean contains(int v) {
 		throw new NotImplementedException();
 	}
-	
+
+	@Override
+	public String toString() {
+		return toString(root);
+	}
+
+	private String toString(Node node) {
+		if (node != null)
+			return node.value + " " + toString(node.left) + " "
+					+ toString(node.right);
+		return "*";
+	}
+
 }
---
+++ src/pucrs/alpro3/arvores/BinarySearchTreeApp.java
@@ -0,0 +1,20 @@
+package pucrs.alpro3.arvores;
+
+public class BinarySearchTreeApp {
+
+	public static void main(String[] args) {
+		BinarySearchTree bst = new BinarySearchTree();
+		
+		System.out.println(bst.size());
+		System.out.println(bst);
+		bst.add(20);
+		System.out.println(bst.size());
+		System.out.println(bst);
+		bst.add(10);
+		System.out.println(bst.size());
+		System.out.println(bst);
+		bst.add(30);
+		System.out.println(bst.size());
+		System.out.println(bst);
+	}
+}
